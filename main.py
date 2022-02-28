4558b245f6eed3cfd0c8daa725ba6adbb4973edf
Jesse Millar <jesse.millar@microsoft.com>
2022-02-28 14:55:44 -0600

    Improving historic message handling

--- main.py
+++ main.py
@@ -10,12 +10,16 @@ async def on_ready():
     print('Logged in as {0.user}'.format(client))
 
     channel = client.get_channel(config["main_channel_id"])
-    all_messages = await channel.history().flatten()
+    all_messages = await channel.history(limit = 100000).flatten()
 
     # Move messages that already have reactions to the log
-    for message in all_messages:
+    i = 0
+    while i < len(all_messages):
+        print("Processing message {0} of {1}".format(i, len(all_messages)))
+        message=all_messages[i]
         if len(message.reactions) > 0:
             await move_message_to_log(message)
+        i += 1
 
 async def move_message_to_log(message):
     message_copy = await client.get_channel(config["log_channel_id"]).send(content = message.content, files = [await f.to_file() for f in message.attachments])
